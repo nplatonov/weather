ebf45b03ac46f1d19e6ff862b5bbdf03fdff689d
nplatonov <nplatonov@yahoo.com>
2024-05-05 19:08:24 +0300

    Ongoing activity

--- devel/daemon.R
+++
@@ -1,5 +0,0 @@
-rmarkdown::run("app.Rmd"
-              ,render_args=list(output_options=list(),quiet=TRUE)
-              ,shiny_args=list(launch.browser=TRUE,port=9992)
-             # ,quiet=!TRUE
-              )
